283154f1fcf511fd4e06f249043cac101645a8ac
Editor T <j17e4eo@icloud.com>
2025-07-25 14:26:33 +0900

    Increase war core slime visibility and HP

--- continent/src/main/java/me/continent/war/WarManager.java
+++ continent/src/main/java/me/continent/war/WarManager.java
@@ -11,7 +11,7 @@ import org.bukkit.Material;
 import java.util.*;
 
 public class WarManager {
-    public static final int VILLAGE_CORE_HP = 20;
+    public static final int VILLAGE_CORE_HP = 100;
     private static final Map<String, War> wars = new HashMap<>();
 
     public static War declareWar(Nation attacker, Nation defender) {
